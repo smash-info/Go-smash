88c6a2a270e00320b62a4056ee7711c4739f09f4
Mason Fish <mason.fish@docker.com>
2016-10-31 20:18:44 -0700

    remove route

--- main.go
+++ main.go
@@ -10,9 +10,7 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
 	r.HandleFunc("/products", ProductsHandler)
-	r.HandleFunc("/articles", ArticlesHandler)
 	fmt.Println("/ – is available")
 	fmt.Println("/products – is available")
-	fmt.Println("/articles – is available")
 	http.Handle("/", r)
 }
