4ac62bf06484e370d8088901b59f477257f91b03
Mason Fish <mason.fish@docker.com>
2016-10-31 20:17:31 -0700

    print out all available routes

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	displayStartup()
@@ -8,5 +11,8 @@ func main() {
 	r.HandleFunc("/", HomeHandler)
 	r.HandleFunc("/products", ProductsHandler)
 	r.HandleFunc("/articles", ArticlesHandler)
+	fmt.Println("/ – is available")
+	fmt.Println("/products – is available")
+	fmt.Println("/articles – is available")
 	http.Handle("/", r)
 }
