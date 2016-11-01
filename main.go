e037001d856329d8881a14308174a32a5763c37f
Mason Fish <mason.fish@docker.com>
2016-10-31 20:07:33 -0700

    add basic mux router and three endpoints

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/products", ProductsHandler)
+	r.HandleFunc("/articles", ArticlesHandler)
+	http.Handle("/", r)
+}
